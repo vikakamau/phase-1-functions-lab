6242de86f7c3f4f6793c659c7a64cc1933b056d5
victor kamau <vikakamau04@gmail.com>
2024-03-20 13:50:06 +0300

    function

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+    const locationHQ= 42
+    return Math.abs(somevalue-42)
+}
+function distanceFromHqInFeet(somevalue){
+    return distanceFromHqInBlocks(somevalue)*264;
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination-start)*264
+}
+function  calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start,destination);
+    if(distance<400){
+        return 0;
+    }else if(distance>400 && distance<=2000){
+        return(distance-400)*0.02
+    }else if(distance>2000 && distance<2500){
+        return 25;
+    }else if(distance>2500){
+        return 'cannot travel that far'
+    }
+}
